b1b1305b1a3410f82da99e0d0106e4dd905a69b6
Junaid <j.ahsan@samsung.com>
2015-04-21 10:33:50 +0600

    this is a sample file to learn how the git works

---
+++ 455.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+int KMP_int(char *string)
+{
+	int len = strlen(string);
+	int i=1,m=-1;
+
+	int table[10000];
+	table[0]=-1;
+
+    while(1)
+	{
+		if(i==len)
+		{
+			return table[i-1];
+			break;
+		}
+
+		for(int j=m+1;j<i;j++)
+		{
+			if(string[i]!=string[j])
+			{
+				table[i]=table[j];
+				break;
+
+			}
+			else
+			{
+				m +=1;
+				table[i]=m;
+				break;
+
+			}
+		}
+
+		
+		++i;
+
+	}
+
+}
+int main()
+{
+	char string[100000];
+	long testCase;
+	cin>>testCase;
+	for(int i=1;i<=testCase;i++)
+	{
+
+		cin>>string;
+
+		int len = strlen(string);
+
+		string[len]='\0';
+			
+		int result = KMP_int(string);
+
+		int temp = len - result - 1;
+
+		if(len%temp==0)
+		{
+			cout<<(temp)<<endl;
+		}
+		else
+		{
+			cout<<len<<endl;
+		}
+
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
